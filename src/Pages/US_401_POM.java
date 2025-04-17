b0ab75c88dcc8d7979afafdd23571b03772ad88e
Telman Bayram <telmanbayram1@gmail.com>
2025-04-18 02:25:10 +0400

    Login checked

--- src/Pages/US_401_POM.java
+++ src/Pages/US_401_POM.java
@@ -1,8 +1,39 @@
 package Pages;
 
+import Utility.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class US_401_POM {
-    public static void main(String[] args) {
-        System.out.println("Merhaba");
+    public US_401_POM() {
+        PageFactory.initElements(BaseDriver.driver,this);
     }
 
+    @FindBy(css = "[href='https://openmrs.org/tr/g%C3%B6steri/']")
+    public WebElement demo;
+
+    @FindBy(css = "[href='#openmrs2']")
+    public WebElement openMrs2;
+
+    @FindBy(css ="[href='https://demo.openmrs.org/openmrs/login.htm']" )
+    public WebElement navigateToLogin;
+
+    @FindBy(css = "[id='username']")
+    public WebElement usernameInput;
+
+    @FindBy(css = "[id='password']")
+    public WebElement passwordInput;
+
+    @FindBy(css = "[id='loginButton']")
+    public WebElement loginButton;
+
+    @FindBy(css = "[id='Inpatient Ward']")
+    public WebElement InpatientWard;
+
+    @FindBy(css = "[id='sessionLocationError']")
+    public WebElement errorMessage;
+
+    @FindBy(css = "[id='error-message']")
+    public WebElement invalidUsername;
 }
--- src/Project/US_401_CheckingLoginErrors.java
+++ src/Project/US_401_CheckingLoginErrors.java
@@ -1,5 +1,47 @@
 package Project;
 
-public class US_401_CheckingLoginErrors {
+import Pages.US_401_POM;
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class US_401_CheckingLoginErrors extends BaseDriver {
+
+    @Test
+    @Parameters({"username","password"})
+    public void Test1(String username, String password){
+        Actions actions=new Actions(driver);
+        US_401_POM pom=new US_401_POM();
+        driver.get("https://openmrs.org/tr/");
+        pom.demo.click();
+        wait.until(ExpectedConditions.elementToBeClickable(pom.openMrs2));
+        actions.scrollToElement(pom.openMrs2).pause(Duration.ofMillis(3)).click(pom.openMrs2).build().perform();
+        wait.until(ExpectedConditions.elementToBeClickable(pom.navigateToLogin));
+        actions.scrollToElement(pom.navigateToLogin).pause(Duration.ofMillis(3)).click(pom.navigateToLogin).build().perform();
+        wait.until(ExpectedConditions.elementToBeClickable(pom.usernameInput));
+        pom.usernameInput.sendKeys(username);
+        pom.passwordInput.sendKeys(password);
+        wait.until(ExpectedConditions.elementToBeClickable(pom.loginButton));
+        pom.loginButton.click();
+        wait.until(ExpectedConditions.visibilityOf(pom.errorMessage));
+        Assert.assertTrue(pom.errorMessage.getText().contains("You must choose a location!"));
+        pom.usernameInput.clear();
+        pom.usernameInput.sendKeys(username);
+        pom.passwordInput.clear();
+        pom.passwordInput.sendKeys(password);
+        wait.until(ExpectedConditions.elementToBeClickable(pom.InpatientWard));
+        actions.click(pom.InpatientWard).pause(Duration.ofMillis(3)).perform();
+        actions.click(pom.loginButton).pause(Duration.ofMillis(3)).perform();
+        wait.until(ExpectedConditions.visibilityOf(pom.invalidUsername));
+        Assert.assertTrue(pom.invalidUsername.getText().contains("Invalid username/password. Please try again."));
+        MyFunc.wait(5);
+
+    }
 
 }
